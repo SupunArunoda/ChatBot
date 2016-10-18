505b9843c6f9c0e7e40294cb40ade9b044094232
Shanalie Silva <shasilva@virtusapolaris.com>
2016-10-18 16:22:06 +0530

    Add JS

---
+++ chat_scripts.js
@@ -0,0 +1,6 @@
+document.getElementById("theButton").addEventListener("click",
+    function() {
+  window.postMessage({ type: "FROM_PAGE", text: "Hello from the webpage!" }, "*");
+  alert("HI");
+}, false);
+
